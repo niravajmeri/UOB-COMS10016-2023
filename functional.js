f5e473983f0a64b8da1c9d968cbdacc2d64ca915
TiloBurghardt <tb2935@bristol.ac.uk>
2022-09-17 14:30:28 +0100

    Update functional.js

--- functional.js
+++ functional.js
@@ -4,8 +4,8 @@ var activityNum   = 8;                         // number of activities per week 
 var columnNum     = 4;                         // desired columns per week (yet, autofitted to max 2 rows per week)
 var title         = "FUNCTIONAL PROGRAMMING";  // content title (different to unitName since multiple content streams maybe in one unit)
 var headerOn      = 1;                         // table column headers on(=1) or off(=0), min of 4 columns needed to render
-var header1       = "Synchronous Sessions";    // leftmost 1x column header
-var header2       = "Asynchronous Materials";  // middle 2x column header
+var header1       = "Extra Exercises";    // leftmost 1x column header
+var header2       = "Lectures";  // middle 2x column header
 var header3       = "Coursework &amp; Labs";  // rest of the columns header
 var inactColour   = "#999999";                 // font colour for inactive content
 var titleColour   = "#777777";                 // table title colour
