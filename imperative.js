e0bbc09b27a39edcacf4f31388d751d2be525ed8
TiloBurghardt <tb2935@bristol.ac.uk>
2022-10-27 18:14:46 +0100

    Update imperative.js

--- imperative.js
+++ imperative.js
@@ -102,11 +102,11 @@ var activities = [
 ["10", "",                     "",                                 "", "", "0", "0"],
 ["12", "",                     "",                                 "", "", "0", "0"],
 ["12", "",                     "",                                 "", "", "0", "0"],
-["7", "in your own time and during labs", "LIST ASSIGNMENT<hr/>Submission Deadline:<br/>Thu 10/11/22<br/>01:00pm UK time<\a>", "https://www.ole.bris.ac.uk/bbcswebdav/courses/COMS10016_2022_TB-1/content/imperative/week06/index.html", "", "91", "3"],
+["7", "in your own time and during labs", "LIST ASSIGNMENT<hr/>", "https://www.ole.bris.ac.uk/bbcswebdav/courses/COMS10016_2022_TB-1/content/imperative/week06/index.html", "", "91", "3"],
 ["10", "",                     "",                                 "", "", "0", "0"],
 ["10", "",                     "",                                 "", "", "0", "0"],
 ["10", "",                     "",                                 "", "", "0", "0"],
-["10", "",                     "",                                 "", "", "0", "0"],
+["8", "",                     "Submission Deadline:<br/>Thu 10/11/22<br/>01:00pm UK time",                                 "", "", "0", "0"],
 
 //WEEK 06 - READING WEEK
 ["17", "Mon 01/11/21<br/>04:00pm<br/>MVB2.11", "Pointer Surgery<br/>with Sion and Tilo",  "https://www.ole.bris.ac.uk/bbcswebdav/courses/COMS10016_2021_TB-1/monday.html", "", "0", "0"],
